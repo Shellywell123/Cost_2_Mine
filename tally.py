bed113cfa55c16fb7a910147cb15cb87d1105167
Shellywell123 <benjamin@shellswell.com>
2021-05-19 23:36:07 +0100

    tally.py date list generator now working correctly

--- tally.py
+++ tally.py
@@ -10,7 +10,7 @@ def get_sheet_names_between(start_date,final_date):
      - looking for way to locate all sheets (shared with me) as the google api makes a sheet and shares it with your gmail account.
     """
 
-
+    # unpack date data
     s_day   = int(start_date.split('/')[0])
     s_month = int(start_date.split('/')[1])
     s_year  = int(start_date.split('/')[2])
@@ -18,6 +18,13 @@ def get_sheet_names_between(start_date,final_date):
     f_day   = int(final_date.split('/')[0])
     f_month = int(final_date.split('/')[1])
     f_year  = int(final_date.split('/')[2])
+
+    # assertion check date data
+    assert(0 < s_day < 32) 
+    assert(0 < f_day < 32) 
+    assert(0 < s_month < 13) 
+    assert(0 < f_month < 13) 
+    assert(f_year >= s_year)
 
     def get_days_in_month(month_num,year):
         """
@@ -29,56 +36,56 @@ def get_sheet_names_between(start_date,final_date):
             num_of_days = [31,28,31,30,31,30,31,31,30,31,30,31]
             print("%d is Not the Leap Year" %year)
 
-        return num_of_days[month_num]
-
-    assert(f_year >= s_year)
+        return num_of_days[month_num-1]
 
     list_of_sheet_names = []
 
-    if s_year==f_year:
-        print('htbgvfd')
+    for year_num in range(s_year,f_year+1):
+
         # then all days across one year
 
+        # set first day of month
+        if year_num == s_year:
+            first_month_of_the_year = s_month
+        else:
+            first_month_of_the_year = 1
+
+        #set last day of month
+        if year_num == f_year:
+            last_month_of_the_year = f_month
+        else:
+            last_month_of_the_year = 12
+
         # generate list of months through
-        for month_num in range(s_month,f_month+1):
-            print('btere')
+        for month_num in range(first_month_of_the_year,last_month_of_the_year+1):
             
             # set first day of month
             if month_num == s_month:
-                first_day_of_the_moth = s_day
+                first_day_of_the_month = s_day
             else:
-                first_day_of_the_moth = 1
+                first_day_of_the_month = 1
 
             #set last day of month
             if month_num == f_month:
                 last_day_of_the_month = f_day
             else:
-                last_day_of_the_month = get_days_in_month(month_num,s_year)     
+                last_day_of_the_month = get_days_in_month(month_num,year_num)     
 
+            assert(type(month_num) == int)
             # generate list of days through each month
-            for day_num in range(first_day_of_the_moth,last_day_of_the_month+1):
-                print('htbegrf')
+            for day_num in range(first_day_of_the_month,last_day_of_the_month+1):
 
                 if day_num < 10:
                     day_num = '0'+str(day_num)
 
+                month_num = int(month_num)
                 if month_num < 10:
                     month_num = '0'+str(month_num)
 
-                sheet_name = "Cost_2_Mine_log _ {}-{}-{}.csv".format(day_num,month_num,f_year)
+                sheet_name = "Cost_2_Mine_log _ {}-{}-{}.csv".format(day_num,month_num,year_num)
+                
+                #print(sheet_name)
                 list_of_sheet_names.append(sheet_name)
-
-    else:
-        # days span across more than one year
-        # todo
-        pass
-
-
-
-    # need to think of a way to assert strart date is beofre final date
-
-    # need to find a way to create a list of dates between any two dates
-
 
     return list_of_sheet_names
 
@@ -112,29 +119,27 @@ def get_mean_price_per_day(sheet_name):
 
 # download/ open google sheets
 
-start_date = '15/05/2020'
-final_date = '16/05/2020'
-#sheet_names = get_sheets_names_between(start_date,final_date)
+start_date = '15/05/2021'
+final_date = '18/05/2021'
+
 sheet_names = get_sheet_names_between(start_date,final_date)
-# cost_total = 0
+cost_total = 0
 
-# for sheet_name in sheet_names:
+for sheet_name in sheet_names:
 
-#     # scrape sheet data and average price of electricy per day for that day
-#     avg = get_mean_price_per_day(sheet_name)
+    # scrape sheet data and average price of electricy per day for that day
+    avg = get_mean_price_per_day(sheet_name)
+    sheet_name,
+    print(sheet_name,avg)
 
-#     # cummaltively make a sum 
-#     cost_total += avg
-
-
-# from Cost_2_Mine import get_live_binance_spot_price
-# tp = (get_live_binance_spot_price('DOGEGBP'))['price']
-# coin_total = float(tp)*cost_total
+    # cummaltively make a sum 
+    cost_total += avg
 
 
-# print('total electricty bill for period {}-{} is £{} or {} DOGE coin'.format(start_date,final_date,cost_total,coin_total))
+from Cost_2_Mine import get_live_binance_spot_price
+tp = (get_live_binance_spot_price('DOGEGBP'))['price']
+coin_total = float(tp)*cost_total
 
-# # get value of electricty bill in terms of current ticker price
 
+print('total electricty bill for period {}-{} is £{} or {} DOGE coin'.format(start_date,final_date,cost_total,coin_total))
 
-print(sheet_names)
